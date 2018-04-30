6672733905e2048a5f31798f27abf7d5744ce4d8
Christopher Herrera <cth1011@yahoo.com>
2018-04-30 23:16:03 +0800

    Add files via upload

--- scrolling.js
+++ scrolling.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#nav').localScroll(800);
   $('#silhouette1').parallax("50%", 0.2);
   $('#silhouette2').parallax("50%", 0.1);
-  $('#intro-text').parallax("50%", 0.3);
+  $('').parallax("50%", 0.1);
   $('#second').parallax("50%", 0.1);
   $('.bg').parallax("50%", 0.4);
   $('#third').parallax("50%", 0.3);
